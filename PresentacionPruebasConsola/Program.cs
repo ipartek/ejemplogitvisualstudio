7276d4fd847489a0bfc0ae3181e558b3d9f1f8be
javierlete <jglete@gmail.com>
2018-02-09 13:47:30 +0100

    Comprobación de biblioteca pruebas con el método sumar en presentación de consola

--- PresentacionPruebasConsola/Program.cs
+++ PresentacionPruebasConsola/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Biblioteca;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,8 @@ namespace PresentacionPruebasConsola
         static void Main(string[] args)
         {
             Console.WriteLine("Hola desde consola");
+
+            Console.WriteLine(Pruebas.Sumar(1, 2));
         }
     }
 }
