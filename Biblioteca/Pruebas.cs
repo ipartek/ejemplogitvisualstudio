891db3a3538fbd4c1f8fc38b7d2d1c992f53145b
javierlete <jglete@gmail.com>
2018-02-09 12:49:58 +0100

    Biblioteca de pruebas

---
+++ Biblioteca/Pruebas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Pruebas
+    {
+        public static int Sumar(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
